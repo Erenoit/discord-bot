f89f1faa631ff913b6ada83c39b3377b82e23444
Eren Önen <erenoit@protonmail.com>
2021-10-24 11:36:25 +0300

    add: first events

---
+++ src/Events/messageCreate.ts
@@ -0,0 +1,26 @@
+import { Event, Command } from "../Interfaces";
+import { Message } from "discord.js";
+
+export const event: Event = {
+  name: "messageCreate",
+  run: async (client, message: Message) => {
+    if (
+      message.author.bot ||
+      !message.guild     ||
+      !message.content.startsWith(client.config.prefix)
+    ) { return; }
+
+    const [cmd, ...args] = message.content
+      .slice(client.config.prefix.length)
+      .trim()
+      .split(/ +/g);
+
+    if (!cmd) { return; }
+    console.log(`Command: ${cmd}`);
+
+    const command = client.commands.get(cmd.toLowerCase()) || client.aliases.get(cmd.toLowerCase());
+    if (command) { (command as Command).run(client, message, args); }
+  }
+};
+
+
