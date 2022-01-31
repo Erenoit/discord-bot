2c7761f3267a282c3808c59b04a7ae2cd9c08275
Eren Önen <erenoit@protonmail.com>
2022-01-31 13:29:47 +0300

    bugfix: ping and play have same alias

--- src/Commands/others/ping.ts
+++ src/Commands/others/ping.ts
@@ -2,7 +2,7 @@ import { Command } from "../../Interfaces";
 
 export const command: Command = {
   name: "ping",
-  aliases: ["p"],
+  aliases: [],
   run: async (client, message, args) => {
     message.reply("Pong! :stuck_out_tongue_winking_eye:");
   }
