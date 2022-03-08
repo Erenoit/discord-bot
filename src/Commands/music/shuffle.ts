ae1515957cfd85dc9b6e2652e5796737c9ac64b6
Eren Önen <erenoit@protonmail.com>
2022-03-08 23:19:06 +0300

    fix: shuffle command

--- src/Commands/music/shuffle.ts
+++ src/Commands/music/shuffle.ts
@@ -4,7 +4,7 @@ export const command: Command = {
   name: "shuffle",
   description: "Adds song to queue",
   aliases: [],
-  run: (client, message, args) => {
-    client.player.shuffle(message);
+  run: (variables) => {
+    variables.client.player.shuffle(variables);
   }
 }
