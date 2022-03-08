9f18ccb23fb7e7069c373f0fbc1a984e03c80509
Eren Önen <erenoit@protonmail.com>
2022-03-09 00:34:46 +0300

    add: basic Messager class

---
+++ src/Messager/index.ts
@@ -0,0 +1,45 @@
+import { MessageEmbedOptions } from "discord.js"
+import { Variables } from "../Interfaces";
+
+class Messager {
+  private use_embed: boolean = false;
+  private colors = {
+    sucsess: 0x00ff00,
+    error:   0xff0000,
+  };
+
+  public send_err(variables: Variables, content: string, log_text?: string) {
+    const title = "Error";
+    this.send_message(variables, title, content, this.colors.error, log_text);
+  }
+
+  public send_sucsess(variables: Variables, content: string, log_text?: string) {
+    const title = "Sucsess";
+    this.send_message(variables, title, content, this.colors.sucsess, log_text);
+  }
+
+  public send_message(variables: Variables, title: string,
+                      content: string, color: number,
+                      log_text?: string) {
+    const main = variables.type === "Old" ? variables.message : variables.interaction;
+    const msg  = this.use_embed ? { embeds: [this.basic_embed(title, content, color)] }
+                                : { content };
+
+    main.reply(msg);
+
+    if (log_text) {
+      console.log(log_text);
+    }
+  }
+
+  private basic_embed(title: string, description: string, color: number): MessageEmbedOptions {
+    const embed: MessageEmbedOptions = {
+      color,
+      title,
+      description,
+    } 
+    return embed;
+  }
+}
+
+export default Messager;
