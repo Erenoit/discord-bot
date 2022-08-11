a533c2ad3fae61efcafc164b19ddd28267c44020
Eren Önen <erenoit@protonmail.com>
2022-08-11 11:07:08 +0300

    bugfix: bot couldn't read messages and changed to more readable intents decleration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { GatewayIntentBits } from "discord.js";
 import Client from "./Client";
 
-const intents: GatewayIntentBits = 32767;
+const intents = GatewayIntentBits.Guilds
+              | GatewayIntentBits.GuildMembers
+              | GatewayIntentBits.GuildBans
+              | GatewayIntentBits.GuildEmojisAndStickers
+              | GatewayIntentBits.GuildIntegrations
+              | GatewayIntentBits.GuildWebhooks
+              | GatewayIntentBits.GuildInvites
+              | GatewayIntentBits.GuildVoiceStates
+              | GatewayIntentBits.GuildPresences
+              | GatewayIntentBits.GuildMessages
+              | GatewayIntentBits.GuildMessageReactions
+              | GatewayIntentBits.GuildMessageTyping
+              | GatewayIntentBits.DirectMessages
+              | GatewayIntentBits.DirectMessageReactions
+              | GatewayIntentBits.DirectMessageTyping
+              | GatewayIntentBits.MessageContent
+              | GatewayIntentBits.GuildScheduledEvents;
+
 const bot = new Client({
   intents,
   rest: { offset: 0 }
