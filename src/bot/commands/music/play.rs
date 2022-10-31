26d6b1e6ac3ca384d406f5399885da7ef9d0a498
Eren Önen <erenoit@protonmail.com>
2022-10-31 20:04:10 +0300

    add: basic search (selects first one)

--- src/bot/commands/music/play.rs
+++ src/bot/commands/music/play.rs
@@ -1,5 +1,6 @@
 use super::super::{Context, Error};
 use crate::messager;
+use tokio::process::Command;
 
 /// Adds song to queue 
 #[poise::command(slash_command, prefix_command, aliases("p"), category="Music", guild_only)]
@@ -14,7 +15,14 @@ pub async fn play(
         // TODO: handle poisoned mutexes as well
         server.player.lock().await.play(&ctx, song).await;
     } else {
-        messager::send_error(&ctx, "Search support is not ready yet! :P", false).await;
+        // TODO: search and select from resoults
+        let search_count = 1;
+        let list = Command::new("youtube-dl")
+            //.args(["--no-playlist", "--get-title", "--get-id", &format!("ytsearch{}:{}", search_count, song)])
+            .args(["--no-playlist", "--get-id", &format!("ytsearch{}:{}", search_count, song)])
+            .output().await?;
+
+        server.player.lock().await.play(&ctx, format!("https://youtube.com/watch?v={}", String::from_utf8(list.stdout).expect("Valid UTF-8"))).await;
     }
 
     Ok(())
