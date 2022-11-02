40bb44bb544461344009ebb8d946e4f668c9a4ff
Eren Önen <erenoit@protonmail.com>
2022-11-02 17:18:09 +0300

    remove: unused import

--- src/bot/commands/mod.rs
+++ src/bot/commands/mod.rs
@@ -2,11 +2,8 @@ pub mod others;
 pub mod entertainment;
 pub mod music;
 
-use serenity::model::guild::Guild;
-
 type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 
 pub struct Data;
-
 
