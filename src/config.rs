eccd19a70c1b810f2f986e8f2a5ee54a75d7481e
Eren Önen <erenoit@protonmail.com>
2022-11-07 11:42:58 +0300

    refactor: add "BOT_" prefix to all environmental variables for bot

--- src/config.rs
+++ src/config.rs
@@ -18,16 +18,16 @@ impl Config {
         logger::info("Registering Configs");
         _ = dotenv::dotenv();
 
-        logger::secondary_info("token");
-        let token = env::var("TOKEN").expect("Couldn't find the token");
+        logger::secondary_info("Token");
+        let token = env::var("BOT_TOKEN").expect("Couldn't find the token");
 
-        logger::secondary_info("prefix");
-        let prefix = env::var("PREFIX").expect("Couldn't find the prefix");
+        logger::secondary_info("Prefix");
+        let prefix = env::var("BOT_PREFIX").expect("Couldn't find the prefix");
 
-        logger::secondary_info("empty servers hashmap");
+        logger::secondary_info("Servers HashMap");
         let servers = RwLock::new(HashMap::new());
 
-        logger::secondary_info("songbird");
+        logger::secondary_info("Songbird");
         let songbird = Songbird::serenity();
 
         Self { token, prefix, servers, songbird }
