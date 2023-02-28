80ef6246d6512038763a3b1896e446830cf3c45b
Eren Önen <erenoit@protonmail.com>
2023-02-28 22:48:42 +0300

    docs: add docs for functions in `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -45,8 +45,14 @@ use tokio::sync::OnceCell;
 
 pub use crate::bot::Bot;
 
+/// This variable keeps everything about discord bot.
+///
+/// Do not use it as is. Instead use the `init_config()` and/or `get_config()`.
 static CONFIG: OnceCell<Config> = OnceCell::const_new();
 
+/// Generate `Config` struct and pu it in `tokio::sync::OnceCell`.
+///
+/// This should be called *before* doing any other things.
 pub fn init_config() -> Result<()> {
     CONFIG.set(Config::generate()?).map_or_else(
         |_| Err(anyhow!("Couldn't set the config in OnceCell")),
@@ -55,7 +61,14 @@ pub fn init_config() -> Result<()> {
 }
 
 #[macro_use]
+/// Dummy module.
+///
+/// I cannot find any way to use `#[macro_use]`
 mod dummy_module {
+    /// Get `Config` from `tokio::sync::once_cell`.
+    ///
+    /// Because it is generated at the start this whole error handling is
+    /// unnecessary. So, expect can be use without need to check.
     macro_rules! get_config {
         () => {{
             use crate::CONFIG;
